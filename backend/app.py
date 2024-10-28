be54213322e27295a4a03f842c280b269cdd79a5
yashwanthkumar08 <ykyash2k03@gmail.com>
2024-10-28 10:55:57 +0530

    added host

--- backend/app.py
+++ backend/app.py
@@ -161,4 +161,4 @@ def check_session():
 if __name__ == "__main__":
     # port = os.environ.get('PORT', 6000)
     # print('port - ' + str(port))
-    app.run(debug=True, port=int(os.environ.get('PORT', 6000)))
+    app.run(debug=True, port=6000,host="0.0.0.0")
